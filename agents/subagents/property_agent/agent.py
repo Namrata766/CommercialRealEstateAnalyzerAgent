a15456b2b44d0415f4b529eed3c9afdd8b5a8d91
Chelcy Millenika <54374275+Chelcy-millenika@users.noreply.github.com>
2025-12-16 10:32:33 +0530

    Update agent.py

--- agents/subagents/property_agent/agent.py
+++ agents/subagents/property_agent/agent.py
@@ -1,6 +1,6 @@
 from google.adk.agents import LlmAgent
 from google.adk.tools import google_search
-
+from .tools import bigquery_tool
 from .prompt import AGENT_INSTRUCTIONS
 
 
@@ -10,6 +10,6 @@ property_agent = LlmAgent(
     name="property_agent",
     model="gemini-2.5-pro",
     instruction=AGENT_INSTRUCTIONS,
-    tools=[google_search],
+    tools=[bigquery_tool,google_search],
     output_key="property_analysis",
 )
